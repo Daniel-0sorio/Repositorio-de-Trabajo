0d3ae570d49e849cc4b0d4f554f4f2952ac72edb
Daniel-0sorio <dosorio@usm.cl>
2024-11-13 21:50:08 -0300

    Setting modification

--- myworld/Scripts/YourUSM/YourUSM/settings.py
+++ myworld/Scripts/YourUSM/YourUSM/settings.py
@@ -72,21 +72,13 @@ WSGI_APPLICATION = 'YourUSM.wsgi.application'
 
 
 # Database
-# https://docs.djangoproject.com/en/5.1/ref/settings/#databases
-
-#DATABASES = {
-#    'default': {
-#       'ENGINE': 'django.db.backends.sqlite3',
-#       'NAME': BASE_DIR / 'db.sqlite3',
-#   }
-#}
-
+#https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+       'ENGINE': 'django.db.backends.sqlite3',
+       'NAME': BASE_DIR / 'db.sqlite3',
+   }
 }
 
 
