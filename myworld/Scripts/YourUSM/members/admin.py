bd7c58535da8a37e382501cd98ec462fcfbed1e0
Daniel-0sorio <dosorio@usm.cl>
2024-11-13 21:52:55 -0300

    admin modification

--- myworld/Scripts/YourUSM/members/admin.py
+++ myworld/Scripts/YourUSM/members/admin.py
@@ -3,7 +3,4 @@ from .models import Member
 
 # Register your models here.
 
-class MemberAdmin(admin.ModelAdmin):
-  list_display = ("firstname", "lastname", "joined_date",)
-  
-admin.site.register(Member, MemberAdmin)
+admin.site.register(Member)
